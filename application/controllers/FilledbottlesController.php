c3818552cb92b5acb34e44ebc47f89159bf8c3f6
boxfrommars <boxfrommars@boxfrommars-desktop.(none)>
2010-04-06 11:30:24 +0400

    filled bottles view added

--- application/controllers/FilledbottlesController.php
+++ application/controllers/FilledbottlesController.php
@@ -5,9 +5,9 @@ class FilledbottlesController extends Zend_Controller_Action
 
     public function init()
     {
-        $bottles = new Application_Model_FilledBottlesMapper();
+        $filledBottles = new Application_Model_FilledBottlesMapper();
         /* show all bottles */
-        $this->view->bottles = $bottles->fetchAll();
+        $this->view->filledBottles = $filledBottles->fetchAll();
     }
 
     public function indexAction()
