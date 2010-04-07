f99559b54a062d997d3da7733a0fc6719facc5a4
boxfrommars <boxfrommars@boxfrommars-desktop.(none)>
2010-04-07 04:23:06 +0400

    добавлена стена, добавлен js для падающих бутылок. уже немного весело :)

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,8 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $filledBottles = new Application_Model_FilledBottlesMapper();
+        $this->view->filledBottles = $filledBottles->fetchAll();
     }
 
 }
