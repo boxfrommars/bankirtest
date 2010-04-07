1ec7aef174031c27d8836fc947fc6b60057fe5f3
boxfrommars <boxfrommars@boxfrommars-desktop.(none)>
2010-04-07 12:32:34 +0400

    роутеры вынесены из бутстрапа в конфиг

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -14,47 +14,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->headScript()->appendFile('/js/script.js', 'text/javascript');
 	}
     
-    protected function _initRoutes()
-    {
+    protected function _initRoutes(){
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/routes.ini', 'production');
         $front = Zend_Controller_Front::getInstance();
         $router = $front->getRouter();
-        
-        $router->addRoute('bottles',
-                            new Zend_Controller_Router_Route('bottles/:bottleId',
-                                                                array(
-                                                                   'controller' => 'bottles',
-                                                                   'action' => 'view'
-                                                                )
-                                                            )
-        );
-        
-        $router->addRoute('beverages',
-                          new Zend_Controller_Router_Route('beverages/:beverageId',
-                                                                array(
-                                                                    'controller' => 'beverages',
-                                                                    'action' => 'view'
-                                                                )
-                                                            )
-        );
-        
-        $router->addRoute('editbeverages',
-                          new Zend_Controller_Router_Route('beverages/edit/:beverageId',
-                                                                array(
-                                                                    'controller' => 'beverages',
-                                                                    'action' => 'edit'
-                                                                )
-                                                            )
-        );
-        
-        $router->addRoute('deletebeverages',
-                          new Zend_Controller_Router_Route('beverages/delete/:beverageId',
-                                                                array(
-                                                                    'controller' => 'beverages',
-                                                                    'action' => 'delete'
-                                                                )
-                                                            )
-        );
-        
+        $router->addConfig($config, 'routes');
     }
 }
 
